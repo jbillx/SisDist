8e852d8c70035f19045b9d21107b082644795157
jbillx <billymicrosip@gmail.com>
2018-03-06 21:38:05 -0600

    Archivo de practicas descomprimido

--- 8Clase/Archivo.cpp
+++ 8Clase/Archivo.cpp
@@ -2,9 +2,26 @@
 #include <iostream>
 
 Archivo::Archivo(string filename){
-	
+	nombreArchivo = filename;
 }
 
 Archivo::Archivo(string filename, int banderas, mode_t modo){
+	nombreArchivo = filename;
+}
+
+size_t Archivo::lee(size_t nbytes){
+
+}
+
+size_t Archivo::escribe(void &buffer, size_t nbytes){
 	
+}
+
+
+size_t Archivo::obtieneNum_bytes(){
+	return num_bytes;
+}
+
+const char Archivo::get_contenido(){
+	return contenido;
 }
---
+++ Proyecto1/asteroide.h
@@ -0,0 +1,12 @@
+#ifndef ASTEROIDE_H
+#define ASTEROIDE_H
+
+class asteroide
+{
+public:
+	asteroide();
+	~asteroide();
+	
+};
+
+#endif
